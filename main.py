e3db0a8046a9a4704a0ae808cbce5fa72627e2d1
RoyDC <77224003@continental.edu.pe>
2026-06-28 22:44:29 -0500

    subida

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173","https://front-core-rosy.vercel.app"],  # React Vite frontend
+    allow_origins=["http://localhost:5173","https://front-core-theta.vercel.app"],  # React Vite frontend
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
